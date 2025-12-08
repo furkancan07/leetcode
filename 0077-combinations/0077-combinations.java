7ccc15dd16ccdc064d1ffab6e170215a8d0a1888
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-08 19:13:39 +0300

    Time: 27 ms (10.72%), Space: 98 MB (23.01%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> list=new ArrayList<>();
+        List<Integer> path=new ArrayList<>();
+        boolean[] visited=new boolean[n+1];
+        dfs(n,k,list,path,visited,1);
+        return list;
+        
+    }
+    public void dfs(int n,int k,List<List<Integer>> list,List<Integer> path,boolean[] visited,int index){
+        if(path.size()==k){
+            list.add(new ArrayList<>(path));
+        }
+        for(int i=index;i<=n;i++){
+          if(visited[i]) continue;
+          path.add(i);
+          visited[i]=true;
+          dfs(n,k,list,path,visited,i+1);
+          visited[i]=false;
+          path.remove(path.size()-1);
+        }
+    }
+}
