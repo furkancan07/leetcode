9e8ae4071a9014e46e3f6c4890dca07ac622564e
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-08 17:02:11 +0300

    Time: 26 ms (34.16%), Space: 48.9 MB (17.89%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer,Integer> map=new HashMap<>();
+        int count=0;
+        int total=0;
+        map.put(0,1);
+        for(int num : nums){
+            total+=num;
+            if(map.containsKey(total-k)){
+                count+=map.get(total-k);
+            }
+            map.put(total,map.getOrDefault(total,0)+1);
+        }
+        return count;
+    }
+}
