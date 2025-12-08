a5686f9e65ede084169d38b83309476e59005c76
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-08 19:03:53 +0300

    Time: 3 ms (67.74%), Space: 46.8 MB (22.8%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> list=new ArrayList<>();
+        List<Integer> path=new ArrayList<>();
+        boolean[] visited=new boolean[nums.length];
+        Arrays.sort(nums); // aynı olan sayıları yan yana getirmek
+        dfs(nums,list,path,visited);
+        return list;
+    }
+    public void dfs(int[] nums,List<List<Integer>> list,List<Integer> path,boolean[] visited){
+        if(path.size()==nums.length){
+            list.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(visited[i]) continue;
+
+            if(i>0 && nums[i]==nums[i-1] && !visited[i-1]) continue;
+
+            path.add(nums[i]);
+            visited[i]=true;
+            dfs(nums,list,path,visited);
+            visited[i]=false;
+            path.remove(path.size()-1);
+        }
+
+    }
+}
