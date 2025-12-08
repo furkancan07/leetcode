93fb18968c69fe1624fb0c104a77f8be003e2807
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-08 19:39:58 +0300

    Time: 3 ms (86.07%), Space: 50.3 MB (69.81%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,5 +1,24 @@
 class Solution {
-    public int numPairsDivisibleBy60(int[] time) {
+    public int numPairsDivisibleBy602(int[] time) {
+        List<List<Integer>> list=new ArrayList<>();
+        List<Integer> path=new ArrayList<>();
+        dfs(time,list,path,0,0);
+        return list.size();
+        
+    }
+    public void dfs(int[] time,List<List<Integer>> list,List<Integer> path,int index,int count){
+      if(path.size() == 2 && count % 60 == 0){
+        list.add(new ArrayList<>(path));
+        return;
+      }
+      if(path.size()>2) return;
+      for(int i=index;i<time.length;i++){
+        path.add(time[i]);
+        dfs(time,list,path,i+1,count+time[i]);
+        path.remove(path.size()-1);
+      }
+    }
+      public int numPairsDivisibleBy60(int[] time) {
         int result=0;
        /* Map<Integer,Integer> map=new HashMap<>(); // istenilen sayıdan kaç tane var onu alacaz
         for(int i : time){
@@ -10,8 +29,8 @@ class Solution {
         }*/
         int[] count=new int[60];
         for(int i : time){
-            int rem=i%60;
-            int com=(60-rem)%60;
+            int rem=i%60; 
+            int com=(60-rem)%60; 
             result+=count[com];
             count[rem]++;
         }
