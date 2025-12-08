8e053dea4fe17f087a313cc1517fb9628f86869e
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-08 17:27:50 +0300

    Time: 1 ms (91.98%), Space: 42.4 MB (42.72%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+        long ans = 0;
+        while (x != 0) {
+            int remainer = x % 10;
+            ans = (ans * 10) + remainer;
+            x /= 10;
+            if (ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE) return 0;
+        }
+        return (int) ans;
+    }
+}
