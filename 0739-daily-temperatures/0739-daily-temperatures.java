2b09fabf969a63e3e2ecdcf3238032ad1871d3b3
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-08 17:10:57 +0300

    Time: 60 ms (77.58%), Space: 107.8 MB (13.49%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] list=new int[temperatures.length];
+        Stack<Integer> stack=new Stack<>();
+       
+       for(int i=0;i<temperatures.length;i++){
+        while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+            int index=stack.pop();
+            list[index]=i-index;
+        }
+        stack.push(i);
+       }
+        return list;
+    }
+}
