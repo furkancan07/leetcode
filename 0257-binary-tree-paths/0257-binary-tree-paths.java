9c6d8dd3a209679323b5f147b129451da18f16b6
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-08 15:05:59 +0300

    Time: 2 ms (81.41%), Space: 44.1 MB (43.13%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ // Direk String ile yapsak backtrackinge gerek kalmayacak çünkü String imuttable değişmez yani
+ // ama biz list kullandık referan değişeceği için onu backtracking ile eski haline getirmek gerek 
+class Solution {
+   
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result=new ArrayList<>();
+        List<Integer> path=new ArrayList<>();
+        dfs(root,result,path);
+        return result;
+        
+    }
+  
+    public void dfs(TreeNode node,List<String> result,List<Integer> path){
+        if(node==null){   
+            return;
+        }
+        path.add(node.val);
+        
+        if(node!=null && node.left==null && node.right==null){
+            StringBuilder sb=new StringBuilder();
+            for(int i=0;i<path.size()-1;i++){
+                sb.append(path.get(i)+"->");
+            }
+            sb.append(path.get(path.size()-1));
+            result.add(sb.toString());
+        }
+        
+        dfs(node.left,result,path);
+        dfs(node.right,result,path);
+        path.remove(path.size()-1);
+        
+        
+    }
+}
