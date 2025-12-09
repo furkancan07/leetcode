85dee5c6b4d45a5e9c3d1c5facbcf0eb6123b132
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-09 21:29:48 +0300

    Time: 0 ms (100%), Space: 41.7 MB (59.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
       if(n<=2) return n;
-      int a=1;
-      int b=1;
-      int c=a+b;
-      for(int i=3;i<=n;i++){
-        a=b;
-        b=c;
-        c=a+b;
+      int[] dp=new int[n+1];
+      dp[0]=1;
+      dp[1]=1;
+      for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
       }
-      return c;
+      return dp[n];
     }
 }
