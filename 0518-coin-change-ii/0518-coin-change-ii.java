467c8c0127f80d57ff187192808d0e666e42846b
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-09 23:00:43 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int change(int amount, int[] coins) {
+   /* public int change(int amount, int[] coins) {
 
          int[] dp=new int[amount+1];
          dp[0]=1;
@@ -13,5 +13,35 @@ class Solution {
             }
          
         return dp[amount];
+    } */ 
+    
+    public int change(int amount, int[] coins) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        dfs(list, path, coins, amount, 0, 0);
+        return list.size();
+    }
+
+    public void dfs(List<List<Integer>> list,
+                    List<Integer> path,
+                    int[] coins,
+                    int target,
+                    int count,
+                    int index) {
+
+        if (count == target) {
+            list.add(new ArrayList<>(path));
+            return;
+        }
+
+        if (count > target) return;
+
+        for (int i = index; i < coins.length; i++) {
+            path.add(coins[i]);
+            dfs(list, path, coins, target, count + coins[i], i); 
+            path.remove(path.size() - 1);
+        }
     }
 }
+
