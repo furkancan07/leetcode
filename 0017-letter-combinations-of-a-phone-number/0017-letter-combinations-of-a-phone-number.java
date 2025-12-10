7de738c943525d4f7b8f4be04603ec5e3c1234ee
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-10 12:02:52 +0300

    Time: 1 ms (55.53%), Space: 43.4 MB (44.85%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         
-        Map<Character,List<Character>> map=new HashMap<>();
+        Map<Character,String> map=new HashMap<>();
 
-        map.put('2',Arrays.asList('a','b','c'));
-        map.put('3',Arrays.asList('d','e','f'));
-        map.put('4',Arrays.asList('g','h','i'));
-        map.put('5',Arrays.asList('j','k','l'));
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
             
-        map.put('6',Arrays.asList('m','n','o'));
-        map.put('7',Arrays.asList('p','q','r','s'));
-        map.put('8',Arrays.asList('t','u','v'));
-        map.put('9',Arrays.asList('w','x','y','z'));
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
 
      
 
@@ -20,7 +20,7 @@ class Solution {
         dfs(list,sb,digits,0,map);
         return list;
     }
-    public void dfs(List<String> list,StringBuilder sb,String digits,int index,Map<Character,List<Character>> map){
+    public void dfs(List<String> list,StringBuilder sb,String digits,int index,Map<Character,String> map){
         if(sb.length()==digits.length()){
             list.add(sb.toString());
             return;
@@ -28,8 +28,8 @@ class Solution {
         if(sb.length()>digits.length()) return;
         if(index>=digits.length()) return ; 
         char c =digits.charAt(index);
-        List<Character> nums=map.get(c);
-        for(char a : nums){
+        String nums=map.get(c);
+        for(char a : nums.toCharArray()){
             sb.append(a);
             dfs(list,sb,digits,index+1,map);
             sb.deleteCharAt(sb.length()-1);
