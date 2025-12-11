41e6dcd9aca698b720fe1a677aaac25d48b7fda5
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-11 16:01:35 +0300

    Time: 6 ms (32.08%), Space: 46.7 MB (5.3%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i : nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> m:map.entrySet()){
+            if(m.getValue()==1){
+                return  m.getKey();
+            }
+        }
+        return -1;
+      
+        
+    }
+}
