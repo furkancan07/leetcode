e94a46f783f00b8b6127c84ec16b8d9195c3c48c
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-14 12:20:33 +0300

    Time: 1 ms (5.52%), Space: 43.4 MB (21.99%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -5,7 +5,7 @@ class Solution {
            sb.append(d);
            }
         
-        sb=sum(sb.toString(),"1");
+        sb=sum(sb.toString());
         int[] res=new int[sb.length()];
         for(int i=0;i<sb.length();i++){
             res[i]=sb.charAt(i)-'0';
@@ -13,15 +13,14 @@ class Solution {
 
         return res;
     }
-    public StringBuilder sum(String s1,String s2){
+    public StringBuilder sum(String s1){
        int i=s1.length()-1;
-       int j=s2.length()-1;
-       int carry=0;
+       
+       int carry=1;
        StringBuilder sb=new StringBuilder();
-       while(i>=0 || j>=0 || carry!=0){
+       while(i>=0 || carry!=0){
         int sum=carry;
         if(i>=0)  sum+=s1.charAt(i--)-'0';
-        if(j>=0) sum+=s2.charAt(j--)-'0';
         sb.append(sum%10);
         carry=sum/10;
        }
