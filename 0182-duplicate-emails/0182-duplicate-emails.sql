b8bba1e7320c22d56ddcd547479d79e7c04f5e54
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-21 12:57:20 +0300

    Time: 358 ms (51.96%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 # select email as Email from person group by email having  count(*)>1
- select DISTINCT a.email as Email from person a join person b on a.email=b.email where a.id!=b.id
+ select DISTINCT a.email as Email from person a join person b on a.email=b.email and a.id!=b.id
