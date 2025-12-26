bfa6d33cc203740f10ef3c561866326e7c4828a2
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2025-12-26 18:59:14 +0300

    Time: 5 ms (31.54%), Space: 43.5 MB (36.58%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] temp=heights.clone();
+        Arrays.sort(heights);
+        int result=0;
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=temp[i]) result++;
+        }
+        return result;
+        
+    }
+}
