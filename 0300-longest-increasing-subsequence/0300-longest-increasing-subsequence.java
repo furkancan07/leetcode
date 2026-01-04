74c922dfea0733e323e77dcdec58aa8e8252f015
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-01-04 16:12:43 +0300

    Time: 56 ms (28.59%), Space: 46.1 MB (69.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,18 +3,18 @@ class Solution {
         int n=nums.length;
         int[] dp=new int[n];
         dp[0]=1;
+        int max=1;
 
         for(int i=1;i<n;i++){
             dp[i]=1;
             for(int j=0;j<i;j++){
                 if(nums[i]>nums[j]) dp[i]=Math.max(dp[i],dp[j]+1);
+                max=Math.max(max,dp[i]);
             }
         }
-        int max=1;
-
-        for(int i=1;i<n;i++){
-            max=Math.max(max,dp[i]);
-        }
+        
+        
+        
         return max;
     }
 }
