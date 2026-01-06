edef1687f6e2855d60e0efc75be304efc49dc0c0
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-01-06 13:59:14 +0300

    Time: 1 ms (78.92%), Space: 43 MB (6.52%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,18 +1,16 @@
 class Solution {
     Set<Integer> set=new HashSet<>();
     public boolean isHappy(int n) {
-    if(n==1) return true;
-    set.add(n);
-    
-    int result=0;
-    while(n!=0){
-        int a=n%10; // 9  1
-        result+=a*a;// 82 
-        n=n/10;// 1
-    }
-    System.out.println(result);
-    if(set.contains(result)) return false;
-    return isHappy(result);
-        
+        if(n==0) return false;
+        if(n==1) return true;
+        set.add(n);
+        int result=0;
+        while(n!=0){
+            int mod=n%10;
+            result+=mod*mod;
+            n=n/10;
+        }  
+        if(set.contains(result)) return false;
+        return isHappy(result);
     }
 }
