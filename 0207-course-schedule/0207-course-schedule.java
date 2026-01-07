1d169d568d3c90899ab19f13e4838a9281f8b9ee
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-01-08 00:41:01 +0300

    Time: 4 ms (93.02%), Space: 47.2 MB (51.77%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,11 +1,12 @@
 class Solution {
     // topolojilk sıralama 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer,List<Integer>> graph=new HashMap<>();
-        
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<>());
+        }
         
         for(int[] pre : prerequisites){
-            graph.putIfAbsent(pre[1],new ArrayList<>());
             graph.get(pre[1]).add(pre[0]);
         }
         int[] visited=new int[numCourses];
@@ -16,14 +17,12 @@ class Solution {
         }
         return true;
     }
-    public boolean hasCycle(Map<Integer,List<Integer>> graph,int[] visited,int i){
+    public boolean hasCycle(List<List<Integer>> graph,int[] visited,int i){
         if(visited[i]==1) return true;
         if(visited[i]==2) return false;
         visited[i]=1;
-        if (graph.containsKey(i)) {
-            for (int n : graph.get(i)) {
-                if (hasCycle(graph, visited, n)) return true;
-            }
+        for(int n : graph.get(i)){
+            if(hasCycle(graph,visited,n)) return true;
         }
         visited[i]=2;
         return false;
