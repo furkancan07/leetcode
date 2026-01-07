7dd412b74fd4597efb6c41292419037e8ea812a3
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-01-08 01:37:09 +0300

    Time: 1 ms (97.95%), Space: 47.7 MB (10.62%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,18 +1,25 @@
 class Solution {
     int nums[];
     
-    public int pivotIndex(int[] nums) {
+    
+    public int pivotIndex2(int[] nums) {
        this.nums=nums;
-       
-        
         for(int i=0;i<nums.length;i++){
             if(sum(0,i)==sum(i+1,nums.length)) return i;
         }
         return  -1;
+    }
 
-       
-
-        
+    public int pivotIndex(int[] nums) {
+        this.nums=nums;
+        int total=sum(0,nums.length);
+        int left=0;
+        for(int i=0;i<nums.length;i++){
+            if(left==total-left-nums[i]) return i;
+            left+=nums[i];
+        }
+        return -1;
+     
     }
   
     public int sum(int i,int j){
