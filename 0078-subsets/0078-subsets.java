ccb097cb806a32013a227b35e1ae60d9c65ff7ae
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-02-11 01:20:52 +0300

    Time: 1 ms (87.93%), Space: 44.3 MB (55.89%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,15 +1,16 @@
 class Solution {
+   
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list=new ArrayList<>();
-        List<Integer> path=new ArrayList<>();
-        dfs(list,path,nums,0);
-        return list;
-        
+         List<List<Integer>> list=new ArrayList<>();
+         List<Integer> path=new ArrayList<>();
+         dfs(list,path,nums,0);
+         return list;
     }
+     // dene
+    // ilerle
+    // geri dön
     public void dfs(List<List<Integer>> list,List<Integer> path,int[] nums,int index){
-
         list.add(new ArrayList<>(path));
-
         for(int i=index;i<nums.length;i++){
             path.add(nums[i]);
             dfs(list,path,nums,i+1);
