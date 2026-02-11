77d48653a65295ad9ba1246f6a3dd80c7a99dc5a
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-02-12 01:19:02 +0300

    Time: 1 ms (86.27%), Space: 45.4 MB (89.6%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,26 +3,23 @@ class Solution {
         List<List<Integer>> list=new ArrayList<>();
         List<Integer> path=new ArrayList<>();
         boolean[] visited=new boolean[nums.length];
-        dfs(nums,list,path,visited);
+        dfs(list,path,visited,nums);
         return list;
     }
-    public void dfs(int[] nums,List<List<Integer>> list,List<Integer> path,boolean[] visited){
+    // dene ilerle geri dön
+    public void dfs(List<List<Integer>> list,List<Integer> path,boolean[] visited,int[] nums){
         if(path.size()==nums.length){
             list.add(new ArrayList<>(path));
-            return ;
+            return;
         }
         for(int i=0;i<nums.length;i++){
-            if(visited[i]){
-           continue;
-            } 
-              path.add(nums[i]);
-        visited[i]=true;
-            dfs(nums,list,path,visited); // 1 2 3 , 1 
-           
+            if(visited[i]) continue;
+            path.add(nums[i]);
+            visited[i]=true;
+            dfs(list,path,visited,nums);
+            visited[i]=false;
             path.remove(path.size()-1);
-             visited[i]=false;
-            
-
         }
+
     }
 }
