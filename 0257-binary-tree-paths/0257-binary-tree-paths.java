748a6bddd26d16d6e43a5c87ac6a65f9dafeab8c
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-02-15 23:04:11 +0300

    Time: 3 ms (76.41%), Space: 44 MB (89.43%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -20,30 +20,27 @@ class Solution {
    
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> result=new ArrayList<>();
-        List<Integer> path=new ArrayList<>();
-        dfs(root,result,path);
+        StringBuilder sb=new StringBuilder();
+        dfs(root,result,sb);
         return result;
         
     }
   
-    public void dfs(TreeNode node,List<String> result,List<Integer> path){
+    public void dfs(TreeNode node,List<String> result,StringBuilder sb){
         if(node==null){   
             return;
         }
-        path.add(node.val);
+        int len=sb.length();
+        if(node.left!=null || node.right!=null) sb.append(node.val+"->");
         
-        if(node!=null && node.left==null && node.right==null){
-            StringBuilder sb=new StringBuilder();
-            for(int i=0;i<path.size()-1;i++){
-                sb.append(path.get(i)+"->");
-            }
-            sb.append(path.get(path.size()-1));
-            result.add(sb.toString());
+       else if(node!=null && node.left==null && node.right==null){
+              sb.append(node.val);
+              result.add(sb.toString());
         }
         
-        dfs(node.left,result,path);
-        dfs(node.right,result,path);
-        path.remove(path.size()-1);
+        dfs(node.left,result,sb);
+        dfs(node.right,result,sb);
+        sb.setLength(len);
         
         
     }
