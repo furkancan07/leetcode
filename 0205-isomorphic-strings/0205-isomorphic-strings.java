2a117406594b546d5b2fb543c18abb33e6be4870
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-02-21 01:56:13 +0300

    Time: 7 ms (77.75%), Space: 43.6 MB (97.15%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length()!=t.length()) return false;
-        int[] count=new int[26];
-        int[] count2=new int[26];
+        int[] map=new int[128];
+        int[] map2=new int[128];
  
-        for(char c : s.toCharArray()){
-            count[c-'a']++;
-        }    
-        for(char c : t.toCharArray()){
-            count2[c-'a']++;
-        }
+        
 
         for(int i=0;i<s.length();i++){
-             if(count[s.charAt(i)-'a']!=count2[t.charAt(i)-'a']) return false;
+             if(map[s.charAt(i)]!=map2[t.charAt(i)]) return false;
+
+             map[s.charAt(i)]=i+1;
+             map2[t.charAt(i)]=i+1;
         }
 
  
