30408274eabbf58bbb46378ac047d2be771134ab
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-02-22 02:05:33 +0300

    Time: 1 ms (84.17%), Space: 44.5 MB (71.41%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,9 +1,11 @@
 class Solution {
     /*
-    open n,
-    close n
-    string 2n
-     */
+    open
+    close
+    string
+    list
+    */
+    
     public List<String> generateParenthesis(int n) {
         List<String> list=new ArrayList<>();
         StringBuilder sb=new StringBuilder();
@@ -11,22 +13,21 @@ class Solution {
         return list;
         
     }
+    // d i g
     public void dfs(List<String> list,StringBuilder sb,int open,int close,int n){
         if(sb.length()==2*n){
             list.add(sb.toString());
             return;
         }
-
         if(open<n){
-            sb.append('(');
-            dfs(list,sb,open+1,close,n);
-            sb.deleteCharAt(sb.length()-1);
-
+          sb.append('(');
+          dfs(list,sb,open+1,close,n);
+          sb.deleteCharAt(sb.length()-1);
         }
         if(close<open){
-            sb.append(')');
-            dfs(list,sb,open,close+1,n);
-            sb.deleteCharAt(sb.length()-1);
+          sb.append(')');
+          dfs(list,sb,open,close+1,n);
+          sb.deleteCharAt(sb.length()-1);
         }
     }
 }
