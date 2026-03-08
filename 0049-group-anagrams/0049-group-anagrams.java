f5780acba0d4f3e4b24ab03ad834904b0488e6c2
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-03-08 14:42:47 +0300

    Time: 6 ms (99.41%), Space: 49.6 MB (73.53%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -6,9 +6,7 @@ class Solution {
             char[] c=s.toCharArray();
             Arrays.sort(c);
             String str=String.valueOf(c);
-            if(!map.containsKey(str)){
-             map.put(str,new ArrayList<>());
-            }
+            map.putIfAbsent(str,new ArrayList<>());
             map.get(str).add(s);
         }
         return new ArrayList<>(map.values());
