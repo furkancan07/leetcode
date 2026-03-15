84c7e3c94369c98d9c816e486285094b98aeefe5
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-03-16 00:25:43 +0300

    Time: 4 ms (92.72%), Space: 47.3 MB (43.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -7,7 +7,7 @@ class Solution {
         }
         
         for(int[] pre : prerequisites){
-            graph.get(pre[1]).add(pre[0]);
+            graph.get(pre[0]).add(pre[1]);
         }
         int[] visited=new int[numCourses];
         for(int i=0;i<numCourses;i++){
