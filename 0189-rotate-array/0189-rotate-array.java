810bd25ecd34b9f91f5094932d7aad6fb1bc19b3
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-03-17 13:01:37 +0300

    Time: 2 ms (23.99%), Space: 67.5 MB (7.17%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -8,12 +8,16 @@ class Solution {
     }
     public void helper(int index,int[] nums,int k){
         if(index>=nums.length) return;
-        if(index+k<nums.length) {
+         int i=(index+k)%nums.length;
+         nums[i]=clone[index];
+         helper(index+1,nums,k);
+    }
+    /*
+    if(index+k<nums.length) {
             nums[index+k]=clone[index];
             }else{
                 int i=(index+k)%nums.length;
                 nums[i]=clone[index];
             }
-             helper(index+1,nums,k);
-    }
+     */
 }
