f9a72d76338cf1fc98b95e35869bf3a37b274ae9
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-03-17 12:49:58 +0300

    Time: 1 ms (77.7%), Space: 42.3 MB (86.98%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -3,14 +3,14 @@ class Solution {
     public boolean isHappy(int n) {
         if(n==0) return false;
         if(n==1) return true;
-        set.add(n);
+       // set.add(n);
         int result=0;
         while(n!=0){
             int mod=n%10;
             result+=mod*mod;
             n=n/10;
         }  
-        if(set.contains(result)) return false;
+        if(!set.add(result)) return false;
         return isHappy(result);
     }
 }
