9ab11e9647376d22911c7517188914ed258dd7ae
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-04-04 00:11:43 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,31 +1,27 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n = grid.length;
-        if (grid[0][0] != 0 || grid[n-1][n-1] != 0) return -1;
-        int[][] directions={{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
-        Queue<int[]> bfs=new ArrayDeque<>();
-        bfs.offer(new int[]{0,0,1});
-
-        while(!bfs.isEmpty()){
-            int[] array=bfs.poll();
+         int n=grid.length;
+        if(grid[0][0]!=0 || grid[n-1][n-1]!=0) return -1;
+        ArrayDeque<int[]> queue=new ArrayDeque<>();
+        int[][] directions={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+        queue.offer(new int[]{0,0,1});
+        while(!queue.isEmpty()){
+            int[] array=queue.poll();
             int x=array[0];
             int y=array[1];
             int dist=array[2];
-
             if(x==n-1 && y==n-1) return dist;
-
             for(int[] dir : directions){
-             int nx=x+dir[0];
-              int ny=y+dir[1];
-
-              if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]==0){
-                grid[nx][ny]=dist+1;
-                bfs.offer(new int[]{nx,ny,dist+1});
-              }
+                int nx=x+dir[0];
+                int ny=y+dir[1];
+                if(nx>=0 && ny>=0 && nx<n && ny<n && grid[nx][ny]==0){
+                    grid[nx][ny]=dist+1;
+                    queue.offer(new int[]{nx,ny,dist+1});
+                } 
+                
             }
         }
-
-        return -1 ;
         
+        return -1;
     }
 }
