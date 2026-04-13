016226bd06a22f762425afd98c545d280d5792ed
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-04-13 03:15:05 +0300

    Time: 0 ms (100%), Space: 49.4 MB (72.81%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,12 +14,9 @@
  * }
  */
 class Solution {
-    int count=0;
+    
     public int countNodes(TreeNode root) {
         if(root==null) return 0;
-        
-        count=1;
-        
-        return count +countNodes(root.left)+countNodes(root.right);
+        return 1 +countNodes(root.left)+countNodes(root.right);
     }
 }
