7c4ca4a01c6254b9d3561e3f939887a43f220e33
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-07-28 23:29:06 +0300

    Time: 22 ms (90.8%), Space: 48.2 MB (37.6%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,30 @@
+class Solution {
+ public static String smallestPalindrome(String s) {
+        int[] count=new int[26];
+        for(char c : s.toCharArray()){
+            count[c-'a']++;
+        }
+        int first=0;
+        int last=s.length()-1;
+        char[] chars=new char[s.length()];
+        for(int i=0;i<26;i++){
+            while(count[i]>0 && count[i]>=2){
+                char temp=(char)(i+'a');
+
+                    chars[first++]=temp;
+                    chars[last--]=temp;
+                    count[i]-=2;
+            }
+        }
+        for(int i=0;i<26;i++){
+            while(count[i]>0){
+                char temp=(char)(i+'a');
+                chars[first++]=temp;
+                count[i]--;
+            }
+        }
+        return new String(chars);
+
+
+    }
+}
