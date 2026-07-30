d7ae77d56f4b8f4dda8607858ad203d4cfca3136
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-07-30 19:22:59 +0300

    Time: 10 ms (66.86%), Space: 47.9 MB (73.84%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumPushes(String word) {
+         int[] count=new int[26];
+        for(char c:word.toCharArray()){
+            count[c-'a']++;
+        }
+        int result=0;
+        int pushed=1;
+        int keyUsed=0;
+        Arrays.sort(count);
+        int index=25;
+        while (index>=0 && count[index]>0){
+            result+=count[index]*pushed;
+            keyUsed++;
+            if(keyUsed==8){
+                keyUsed=0;
+                pushed++;
+            }
+            index--;
+        }
+        return result;
+    }
+}
