a50c14ea22eb937581175d113b40b0d97310c9ed
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-08-03 22:07:36 +0300

    Time: 1 ms (100%), Space: 42.5 MB (85.4%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,5 +1,22 @@
 class Solution {
-    public int maxProduct(int n) {
+     public int maxProduct(int n) {
+        if(n<100) return (n%10)*(n/10);
+        int temp1=0;
+        int temp2=0;
+        while(n>0){
+          int mod=n%10;
+          if(temp1<mod){
+            temp2=temp1;
+            temp1=mod;
+          }else if(temp2<mod){
+            temp2=mod;
+          }
+          n=n/10;
+        }
+        return temp1*temp2;
+        
+    }
+    public int maxProduct2(int n) {
         int result=1;
         List<Integer> list=new ArrayList<>();
         while(n>0){
