b70a32989d689a6635bf46172deef4178be4905f
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-08-05 18:03:48 +0300

    Time: 11 ms (98.15%), Space: 240.5 MB (97.08%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,6 +1,32 @@
 class Solution {
     int dest;
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
+    // find union en iyi yol
+ public boolean validPath(int n, int[][] edges, int source, int destination) {
+      int[] parent=new int[n];
+      for(int i=0;i<n;i++){
+        parent[i]=i;
+      }
+      for(int[] edge:edges){
+        union(edge[0],edge[1],parent);
+      }
+      return find(source,parent)==find(destination,parent);
+        
+    }
+    public void union(int x,int y,int[] parent){
+       int rootX=find(x,parent);
+       int rootY=find(y,parent);
+       if(rootX!=rootY){
+        parent[rootX]=rootY;
+       }
+    }
+    public int find(int x,int[] parent){
+       if(x!=parent[x]){
+        parent[x]=find(parent[x],parent);
+       }
+       return parent[x];
+    }
+
+    public boolean validPathForBfs(int n, int[][] edges, int source, int destination) {
         boolean[] visited=new boolean[n];
         // map bellekte çok yer kapladığı için time limit hatası aldı
        /* Map<Integer,List<Integer>> graph=new HashMap<>();
@@ -37,7 +63,7 @@ class Solution {
         return false;
         
     }
-    public boolean validPath2(int n, int[][] edges, int source, int destination) {
+    public boolean validPathForDfs(int n, int[][] edges, int source, int destination) {
         boolean[] visited=new boolean[n];
         Map<Integer,List<Integer>> graph=new HashMap<>();
         for(int[] edge : edges){
