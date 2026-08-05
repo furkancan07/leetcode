d980fb6c35e70fb187cf4fb94ec2caaef9eb9cd0
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-08-05 03:18:47 +0300

    Time: 2 ms (97.18%), Space: 48.4 MB (59.99%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -14,7 +14,24 @@
  * }
  */
 class Solution {
-    public List<Double> averageOfLevels(TreeNode root) {
+    public List<Double> averageOfLevels(TreeNode root){
+         Queue<TreeNode> queue=new LinkedList<>();
+        List<Double> avg=new ArrayList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            double sum=0;
+            for(int i=0;i<size;i++){
+                TreeNode node=queue.poll();
+                sum+=node.val;
+                if(node.left!=null)queue.offer(node.left);
+               if(node.right!=null) queue.offer(node.right);
+            }
+avg.add(sum/size);
+        }
+        return avg;
+    }
+    public List<Double> averageOfLevels2(TreeNode root) {
        List<List<Integer>> list=new ArrayList<>();
        int depth=0;
        dfs(root,list,depth);
