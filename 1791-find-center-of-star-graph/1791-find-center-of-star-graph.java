4d88014a85d71cc5c7bb30382d5206df88530a3b
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-08-06 23:14:49 +0300

    Time: 32 ms (8.01%), Space: 130.2 MB (6.93%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,5 +1,25 @@
 class Solution {
-    public int findCenter(int[][] edges) {
+    // map ile bulma freq ile aynı algoritma
+     public int findCenter(int[][] edges) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int[] edge : edges){
+            for(int i : edge){
+                map.put(i,map.getOrDefault(map.get(i),0)+1);
+            }
+        }
+        int max=0;
+        int result=0;
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if(entry.getValue()>max){
+                max=entry.getValue();
+                result=entry.getKey();
+            }
+        }
+
+            return result;
+    }
+    // freq ile bulma 
+    public int findCenter2(int[][] edges) {
         int[] count=new int[100001];
         for(int[] edge : edges){
             for(int i : edge){
