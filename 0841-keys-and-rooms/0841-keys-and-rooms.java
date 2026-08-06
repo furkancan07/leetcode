e26e6451f97eb522c78849f8681fbaac21334e60
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-08-06 21:18:24 +0300

    Time: 2 ms (45.12%), Space: 46.5 MB (11.63%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,5 +1,27 @@
 class Solution {
+    // bds
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n=rooms.size();
+        boolean[] visited=new boolean[n];
+
+        Queue<Integer> queue=new ArrayDeque<>();
+        queue.offer(0);
+        visited[0]=true;
+        while(!queue.isEmpty()){
+            int current=queue.poll();
+            for(int i : rooms.get(current)){
+                if(!visited[i]){
+                    visited[i]=true;
+                    queue.offer(i);
+                }
+            }
+        }
+          for(boolean b : visited){
+            if(b==false) return false;
+        }
+        return true;
+    }
+    public boolean canVisitAllRoomsForDfs(List<List<Integer>> rooms) {
         int n=rooms.size();
         boolean[] visited=new boolean[n];
 
@@ -9,8 +31,6 @@ class Solution {
             if(b==false) return false;
         }
         return true;
-
-        
     }
     public void dfs(int room,List<List<Integer>> rooms,boolean[] visited){
         if(visited[room]) return;
