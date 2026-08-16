2e3d95be3e672b90722cc973bb746aa3fa258a35
Furkan Can <101043567+furkancan07@users.noreply.github.com>
2026-08-17 00:09:28 +0300

    Time: 1 ms (2.62%), Space: 42.1 MB (53.03%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -1,6 +1,16 @@
 class Solution {
-    public int bulbSwitch(int n) {
+   public int bulbSwitch(int n) {
+        int count=0;
+        for(int i=1;i*i<=n;i++){
+            if(i*i<=n) count++;
+        }
+        return count;
+        
+    }
+
+    public int bulbSwitchfromUtilsMethod(int n) {
         return (int) Math.sqrt(n);
         
     }
+
 }
